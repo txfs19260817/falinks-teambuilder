03d659278f0f57ed53cce20fb0b9e3c8b6d5281d
txfs19260817 <duola865@gmail.com>
2022-04-25 14:21:39 -0700

    feat: navbar

---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,76 @@
+import Link from 'next/link';
+
+import { AppConfig } from '@/utils/AppConfig';
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-neutral text-neutral-content">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          <ul tabIndex={0} className="dropdown-content menu rounded-box bg-neutral p-2 text-neutral-content shadow">
+            <li>
+              <Link href="/about">
+                <a className="border-none">About</a>
+              </Link>
+            </li>
+
+            <li tabIndex={0}>
+              <a className="justify-between">
+                Parent
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="bg-neutral text-neutral-content">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <Link href="/">
+          <a className="btn btn-ghost text-xl normal-case">{AppConfig.title}</a>
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal">
+          <li>
+            <Link href="/about">
+              <a className="border-none">About</a>
+            </Link>
+          </li>
+          <li tabIndex={0}>
+            <a className="hover:border-none">
+              Parent
+              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="rounded-box bg-neutral text-neutral-content">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-primary">Get started</a>
+      </div>
+    </div>
+  );
+};
+
+export { Navbar };
---
+++ src/utils/Routes.ts
@@ -0,0 +1,8 @@
+export const Routes = [
+  {
+    path: '/',
+  },
+  {
+    path: '/about',
+  },
+];
