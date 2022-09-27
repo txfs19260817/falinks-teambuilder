6f0d233c97074b4643961dfecba6e5000c64185c
txfs19260817 <duola865@gmail.com>
2022-09-27 10:27:25 -0700

    test: utils

---
+++ __tests__/utils/Helpers.test.ts
@@ -0,0 +1,111 @@
+import {
+  convertObjectNumberValuesToFraction,
+  convertStylesStringToObject,
+  ensureInteger,
+  filterSortLimitObjectByValues,
+  fractionToPercentage,
+  getRandomColor,
+  invertColor,
+  S4,
+  urlPattern,
+} from '@/utils/Helpers';
+
+describe('S4', () => {
+  it('should return a string of length 4', () => {
+    const target = S4();
+    expect(typeof target).toBe('string');
+    expect(target.length).toBe(4);
+  });
+
+  it('should return a string that is a valid hexadecimal number', () => {
+    expect(S4()).toMatch(/^[0-9a-f]+$/);
+  });
+});
+
+describe('convertStylesStringToObject', () => {
+  it('should return an object with the correct properties', () => {
+    expect(convertStylesStringToObject('color: red; font-size: 12px;')).toEqual({
+      color: 'red',
+      fontSize: '12px',
+    });
+  });
+});
+
+describe('getRandomColor', () => {
+  it('should return a string that is a valid hexadecimal color', () => {
+    expect(getRandomColor()).toMatch(/^#[0-9a-f]{6}$/);
+  });
+});
+
+describe('invertColor', () => {
+  it('should return #000000 when the color is light', () => {
+    expect(invertColor('#FFFFFF')).toBe('#000000');
+  });
+
+  it('should return #FFFFFF when the color is dark', () => {
+    expect(invertColor('#000000')).toBe('#FFFFFF');
+  });
+});
+
+describe('urlPattern', () => {
+  it('should match a valid URL', () => {
+    expect('https://www.google.com').toMatch(urlPattern);
+    expect('google.com').toMatch(urlPattern);
+  });
+
+  it('should not match an invalid URL', () => {
+    expect('google').not.toMatch(urlPattern);
+  });
+});
+
+describe('ensureInteger', () => {
+  it('should return a number when given a number', () => {
+    expect(ensureInteger(1)).toBe(1);
+  });
+
+  it('should return a number when given a string', () => {
+    expect(ensureInteger('123')).toBe(123);
+  });
+
+  it('should return a default number when given an invalid value', () => {
+    expect(ensureInteger('a', 2)).toBe(2);
+  });
+});
+
+describe('filterSortLimitObjectByValues', () => {
+  it('should return an object with the correct properties', () => {
+    const target = {
+      a: 1,
+      b: 2,
+      d: 4,
+      c: 3,
+      e: 5,
+    };
+    expect(
+      filterSortLimitObjectByValues(
+        target,
+        (v) => v >= 2,
+        (a, b) => b - a,
+        2
+      )
+    ).toEqual({
+      d: 4,
+      e: 5,
+    });
+  });
+});
+
+describe('convertObjectNumberValuesToFraction', () => {
+  it('should return an object with the correct properties', () => {
+    expect(convertObjectNumberValuesToFraction({ a: 1, b: 3 })).toEqual({
+      a: 0.25,
+      b: 0.75,
+    });
+  });
+});
+
+describe('fractionToPercentage', () => {
+  it('should return a string with 1 decimal place', () => {
+    expect(fractionToPercentage(0.5)).toBe('50.0%');
+  });
+});
--- src/utils/PokemonUtils.ts
+++ src/utils/PokemonUtils.ts
@@ -11,7 +11,27 @@ import type { Usage } from '@/utils/Types';
 import { Spreads } from '@/utils/Types';
 
 const maxTotalEvs = 508;
+
 const maxSingleEvs = 252;
+
+export const defaultStats: StatsTable = {
+  hp: 0,
+  atk: 0,
+  def: 0,
+  spa: 0,
+  spd: 0,
+  spe: 0,
+};
+
+export const defaultIvs: StatsTable = {
+  hp: 31,
+  atk: 31,
+  def: 31,
+  spa: 31,
+  spd: 31,
+  spe: 31,
+};
+
 export const typesWithEmoji = [
   { type: 'Bug', emoji: '🐞' },
   { type: '???', emoji: '❓' },
@@ -34,97 +54,6 @@ export const typesWithEmoji = [
   { type: 'Water', emoji: '💧' },
 ];
 
-/**
- * Returns the icon for the given Pokémon.
- * @param {number} pokeNum - The No. of the Pokémon. Only required if fetching from local (fromPS = false).
- * @param {string} pokeName - The name of the Pokémon. Only required if fetching from Pokémon Showdown site (fromPS = true).
- * @param {boolean} fromPS - Fetch icons from the Pokémon Showdown site if true, otherwise from this site.
- */
-export const getPokemonIcon = (pokeNum?: number, pokeName?: string, fromPS?: boolean): Record<string, string> => {
-  if (fromPS && pokeName) {
-    return Icons.getPokemon(pokeName).css;
-  }
-  let num = pokeNum ?? 0;
-  if (num < 0 || num > 898) num = 0;
-
-  const top = -Math.floor(num / 12) * 30;
-  const left = -(num % 12) * 40;
-
-  const url = `/assets/sprites/pokemonicons-sheet.png`;
-  return {
-    display: 'inline-block',
-    width: '40px',
-    height: '30px',
-    imageRendering: 'pixelated',
-    background: `transparent url(${url}) no-repeat scroll ${left}px ${top}px`,
-  };
-};
-
-export const getStats = (stat: string, base: number, ev: number, iv: number, nature: Nature, level: number = 50): number => {
-  return stat === 'hp'
-    ? Math.floor((Math.floor(2 * base + iv + Math.floor(ev / 4) + 100) * level) / 100 + 10)
-    : Math.floor(((Math.floor(2 * base + iv + Math.floor(ev / 4)) * level) / 100 + 5) * (nature.plus === stat ? 1.1 : nature.minus === stat ? 0.9 : 1));
-};
-
-// 252 or left over EVs
-export const getSingleEvUpperLimit = (evs: StatsTable, oldEv: number): number => {
-  return Math.min(maxTotalEvs - Object.values(evs).reduce((x, y) => x + y, 0) + oldEv, maxSingleEvs);
-};
-
-export const getRandomTrainerName = () => getRandomElement(trainerNames) || 'Trainer';
-
-// get the raw usage statistics from the Smogon API for the given format
-export const getLatestUsageByFormat: (format?: string) => Promise<UsageStatistics> = async (format: string = AppConfig.defaultFormat) => {
-  const { url, latestDate, process } = Statistics;
-  const latest = await latestDate(format, true);
-  const year = new Date().getFullYear();
-  const month = new Date().getMonth(); // 0-11
-  const date = latest?.date ?? month === 0 ? `${year}-${`${month}`.padStart(2, '0')}` : `${year - 1}-12`;
-  return process(await fetch(url(date, format)).then((r) => r.text()));
-};
-
-// filter, sort, and limit the Abilities, Items, Spreads, Teammates and Moves of each Pokémon's usage statistics
-export const trimUsage = (
-  oldUsage: MovesetStatistics & { name: string },
-  rank: number,
-  threshold: number = 0.01,
-  comparator: (a: number, b: number) => number = (a, b) => b - a,
-  limit: number = 15
-): Usage => {
-  const { Abilities, Items, Spreads: freqSpreads, Teammates, Moves } = oldUsage;
-  const newAbilities = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(Abilities), (_) => true, comparator, limit);
-  const newItems = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(Items), (_) => true, comparator, limit);
-  const newSpreads = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(freqSpreads), (v) => v >= threshold, comparator, limit);
-  const newTeammates = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(Teammates), (v) => v >= threshold, comparator, limit);
-  const newMoves = filterSortLimitObjectByValues(
-    convertObjectNumberValuesToFraction(Moves),
-    (v) => v > threshold * 0.1,
-    (a, b) => b - a,
-    limit
-  );
-  delete newItems.nothing;
-  delete newMoves['']; // remove the empty move
-  return {
-    ...oldUsage,
-    rank,
-    Abilities: newAbilities,
-    Items: newItems,
-    Moves: newMoves,
-    Spreads: newSpreads,
-    Teammates: newTeammates,
-  };
-};
-
-// convert the raw usage statistics to a more usable format
-export const postProcessUsage = (format: string = AppConfig.defaultFormat): Promise<Usage[]> => {
-  return getLatestUsageByFormat(format).then((r) =>
-    Object.entries(r.data)
-      .map(([name, obj]) => ({ name, ...obj }))
-      .sort((a, b) => b.usage - a.usage)
-      .map((u, i) => trimUsage(u, i))
-  );
-};
-
 const hyphenNameToWikiName = new Map<string, string>([
   ['mr-rime', 'Mr. Rime'],
   ['mime-jr', 'Mime Jr.'],
@@ -140,75 +69,6 @@ const hyphenNameToWikiName = new Map<string, string>([
   ['kommo-o', 'Kommo-o'],
   ['will-o-wisp', 'Will-O-Wisp'],
 ]);
-
-export const wikiLink = (keyword: string) => {
-  const lowerCaseKeyword = keyword.toLowerCase();
-  if (hyphenNameToWikiName.has(lowerCaseKeyword)) {
-    return `https://bulbapedia.bulbagarden.net/wiki/${hyphenNameToWikiName.get(lowerCaseKeyword)}`;
-  }
-  return `https://bulbapedia.bulbagarden.net/wiki/${keyword
-    .split(/[-:]/)
-    .at(0)!
-    .split(' ')
-    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-    .join('_')}`;
-};
-
-export const getMovesBySpecie = (gen: Generation, speciesName?: string): Promise<Move[]> => {
-  return gen.learnsets.get(speciesName || '').then(async (l) => {
-    const res = Object.entries(l?.learnset ?? []).flatMap((e) => gen.moves.get(e[0]) ?? []);
-    const baseSpecies = gen.species.get(speciesName || '')?.baseSpecies ?? '';
-    if (baseSpecies !== speciesName && baseSpecies !== '') {
-      const baseSpeciesMoves = await getMovesBySpecie(gen, baseSpecies);
-      res.push(...baseSpeciesMoves);
-    }
-    return res;
-  });
-};
-
-export const defaultStats: StatsTable = {
-  hp: 0,
-  atk: 0,
-  def: 0,
-  spa: 0,
-  spd: 0,
-  spe: 0,
-};
-
-export const defaultIvs: StatsTable = {
-  hp: 31,
-  atk: 31,
-  def: 31,
-  spa: 31,
-  spd: 31,
-  spe: 31,
-};
-
-// process the fetched usage statistics into a some options for the user to choose from
-export const getSuggestedSpreadsBySpecie = (d: DisplayUsageStatistics & LegacyDisplayUsageStatistics): Spreads[] =>
-  Object.keys(
-    filterSortLimitObjectByValues(
-      d.stats ?? d.spreads ?? {}, // its either in the stats (DisplayUsageStatistics) or spreads (LegacyDisplayUsageStatistics) field
-      (v) => v > 0.001, // only show spreads with a usage of 0.1% or higher
-      (a, b) => b - a, // sort by usage descending
-      5 // only show the top 5 spreads
-    )
-  ).map(
-    (s) =>
-      ({
-        label: s, // the label is the spread itself
-        nature: s.split(':')[0], // the nature is the first part of the spread
-        evs: Object.fromEntries(
-          // parse the string like "Adamant:252/0/0/0/4/252" into an StatsTable like {hp: 0, atk: 252, def: 0, spa: 0, spd: 4, spe: 252}
-          s
-            .split(':')[1]!
-            .split('/')
-            .map((e: string, i: number) =>
-              i === 0 ? ['hp', +e] : i === 1 ? ['atk', +e] : i === 2 ? ['def', +e] : i === 3 ? ['spa', +e] : i === 4 ? ['spd', +e] : ['spe', +e]
-            )
-        ),
-      } as Spreads)
-  );
 
 export const defaultSuggestedSpreads: Spreads[] = [
   {
@@ -272,3 +132,187 @@ export const defaultSuggestedSpreads: Spreads[] = [
     label: 'Specially Defensive: 252 HP / 4 Def / 252 SpD / (+SpD, -Atk)',
   },
 ];
+
+/**
+ * Returns the icon for the given Pokémon.
+ * @param {number} pokeNum - The No. of the Pokémon. Only required if fetching from local (fromPS = false).
+ * @param {string} pokeName - The name of the Pokémon. Only required if fetching from Pokémon Showdown site (fromPS = true).
+ * @param {boolean} fromPS - Fetch icons from the Pokémon Showdown site if true, otherwise from this site.
+ */
+export const getPokemonIcon = (pokeNum?: number, pokeName?: string, fromPS?: boolean): Record<string, string> => {
+  if (fromPS && pokeName) {
+    return Icons.getPokemon(pokeName).css;
+  }
+  let num = pokeNum ?? 0;
+  if (num < 0 || num > 898) num = 0;
+
+  const top = -Math.floor(num / 12) * 30;
+  const left = -(num % 12) * 40;
+
+  const url = `/assets/sprites/pokemonicons-sheet.png`;
+  return {
+    display: 'inline-block',
+    width: '40px',
+    height: '30px',
+    imageRendering: 'pixelated',
+    background: `transparent url(${url}) no-repeat scroll ${left}px ${top}px`,
+  };
+};
+
+/**
+ * Returns the stats for the given base stats, evs, ivs, nature, and level.
+ * @param stat - The stat to calculate. Must be one of 'hp', 'atk', 'def', 'spa', 'spd', or 'spe'.
+ * @param base - The base stat of the Pokémon.
+ * @param ev - The EVs of the Pokémon. Must be between 0 and 252.
+ * @param iv - The IVs of the Pokémon. Must be between 0 and 31.
+ * @param nature - The nature of the Pokémon. E.g. 'Adamant', 'Brave'.
+ * @param level - The level of the Pokémon. Must be between 1 and 100. Defaults to 50.
+ */
+export const getStats = (stat: string, base: number, ev: number, iv: number, nature: Nature, level: number = 50): number => {
+  return stat === 'hp'
+    ? Math.floor((Math.floor(2 * base + iv + Math.floor(ev / 4) + 100) * level) / 100 + 10)
+    : Math.floor(((Math.floor(2 * base + iv + Math.floor(ev / 4)) * level) / 100 + 5) * (nature.plus === stat ? 1.1 : nature.minus === stat ? 0.9 : 1));
+};
+
+/**
+ * Calculates the upper bound of EVs that can be put into a stat.
+ * @param evs - The current EVs of the Pokémon.
+ * @param oldEv - The old EV of the stat which is being changed.
+ */
+export const getSingleEvUpperLimit = (evs: StatsTable, oldEv: number): number => {
+  // 252 or left over EVs
+  return Math.min(maxTotalEvs - Object.values(evs).reduce((x, y) => x + y, 0) + oldEv, maxSingleEvs);
+};
+
+/**
+ * Randomly generates a Pokémon trainer name.
+ */
+export const getRandomTrainerName = () => getRandomElement(trainerNames) || 'Trainer';
+
+/**
+ * Get the raw usage statistics from the Smogon API for the given format
+ * @param format - The format to get usage statistics for. Defaults to `AppConfig.defaultFormat`.
+ */
+export const getLatestUsageByFormat: (format?: string) => Promise<UsageStatistics> = async (format: string = AppConfig.defaultFormat) => {
+  const { url, latestDate, process } = Statistics;
+  const latest = await latestDate(format, true);
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth(); // 0-11
+  const date = latest?.date ?? month === 0 ? `${year}-${`${month}`.padStart(2, '0')}` : `${year - 1}-12`;
+  return process(await fetch(url(date, format)).then((r) => r.text()));
+};
+
+/**
+ * Filters, sorts, and limits the Abilities, Items, Spreads, Teammates and Moves of each Pokémon's usage statistics.
+ * @param oldUsage - The usage statistics to filter, sort, and limit of each Pokémon.
+ * @param rank - The rank of the Pokémon in the usage statistics.
+ * @param threshold - The minimum usage percentage of Abilities, Items, Spreads, Teammates of the Pokémon to be included in the filtered usage statistics.
+ * @param comparator - The comparator function to sort the filtered usage statistics.
+ * @param limit - The maximum number of Abilities, Items, Spreads, Teammates of the Pokémon to be included in the filtered usage statistics.
+ */
+export const trimUsage = (
+  oldUsage: MovesetStatistics & { name: string },
+  rank: number,
+  threshold: number = 0.01,
+  comparator: (a: number, b: number) => number = (a, b) => b - a,
+  limit: number = 15
+): Usage => {
+  const { Abilities, Items, Spreads: freqSpreads, Teammates, Moves } = oldUsage;
+  const newAbilities = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(Abilities), (_) => true, comparator, limit);
+  const newItems = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(Items), (_) => true, comparator, limit);
+  const newSpreads = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(freqSpreads), (v) => v >= threshold, comparator, limit);
+  const newTeammates = filterSortLimitObjectByValues(convertObjectNumberValuesToFraction(Teammates), (v) => v >= threshold, comparator, limit);
+  const newMoves = filterSortLimitObjectByValues(
+    convertObjectNumberValuesToFraction(Moves),
+    (v) => v > threshold * 0.1,
+    (a, b) => b - a,
+    limit
+  );
+  delete newItems.nothing;
+  delete newMoves['']; // remove the empty move
+  return {
+    ...oldUsage,
+    rank,
+    Abilities: newAbilities,
+    Items: newItems,
+    Moves: newMoves,
+    Spreads: newSpreads,
+    Teammates: newTeammates,
+  };
+};
+
+/**
+ * Convert the raw usage statistics to a more usable format
+ * @param format - The format to get usage statistics for. Defaults to `AppConfig.defaultFormat`.
+ */
+export const postProcessUsage = (format: string = AppConfig.defaultFormat): Promise<Usage[]> => {
+  return getLatestUsageByFormat(format).then((r) =>
+    Object.entries(r.data)
+      .map(([name, obj]) => ({ name, ...obj }))
+      .sort((a, b) => b.usage - a.usage)
+      .map((u, i) => trimUsage(u, i))
+  );
+};
+
+/**
+ * Generates a Wiki link for the given name of Pokémon, item, ability, move, or nature.
+ * @param keyword
+ */
+export const wikiLink = (keyword: string) => {
+  const lowerCaseKeyword = keyword.toLowerCase();
+  if (hyphenNameToWikiName.has(lowerCaseKeyword)) {
+    return `https://bulbapedia.bulbagarden.net/wiki/${hyphenNameToWikiName.get(lowerCaseKeyword)}`;
+  }
+  return `https://bulbapedia.bulbagarden.net/wiki/${keyword
+    .split(/[-:]/)
+    .at(0)!
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join('_')}`;
+};
+
+/**
+ * Retrieves learnable moves for the given Pokémon in the latest generation.
+ * @param gen
+ * @param speciesName
+ */
+export const getMovesBySpecie = (gen: Generation, speciesName?: string): Promise<Move[]> => {
+  return gen.learnsets.get(speciesName || '').then(async (l) => {
+    const res = Object.entries(l?.learnset ?? []).flatMap((e) => gen.moves.get(e[0]) ?? []);
+    const baseSpecies = gen.species.get(speciesName || '')?.baseSpecies ?? '';
+    if (baseSpecies !== speciesName && baseSpecies !== '') {
+      const baseSpeciesMoves = await getMovesBySpecie(gen, baseSpecies);
+      res.push(...baseSpeciesMoves);
+    }
+    return res;
+  });
+};
+
+/**
+ * Processes the fetched usage statistics into a some options for the user to choose from.
+ * @param d - The fetched usage statistics.
+ */
+export const getSuggestedSpreadsBySpecie = (d: DisplayUsageStatistics & LegacyDisplayUsageStatistics): Spreads[] =>
+  Object.keys(
+    filterSortLimitObjectByValues(
+      d.stats ?? d.spreads ?? {}, // its either in the stats (DisplayUsageStatistics) or spreads (LegacyDisplayUsageStatistics) field
+      (v) => v > 0.001, // only show spreads with a usage of 0.1% or higher
+      (a, b) => b - a, // sort by usage descending
+      5 // only show the top 5 spreads
+    )
+  ).map(
+    (s) =>
+      ({
+        label: s, // the label is the spread itself
+        nature: s.split(':')[0], // the nature is the first part of the spread
+        evs: Object.fromEntries(
+          // parse the string like "Adamant:252/0/0/0/4/252" into an StatsTable like {hp: 0, atk: 252, def: 0, spa: 0, spd: 4, spe: 252}
+          s
+            .split(':')[1]!
+            .split('/')
+            .map((e: string, i: number) =>
+              i === 0 ? ['hp', +e] : i === 1 ? ['atk', +e] : i === 2 ? ['def', +e] : i === 3 ? ['spa', +e] : i === 4 ? ['spd', +e] : ['spe', +e]
+            )
+        ),
+      } as Spreads)
+  );
