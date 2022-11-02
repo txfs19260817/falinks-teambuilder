7af6b10fa8b817cc0eb520c5d9e9ccaf5f623c82
txfs19260817 <duola865@gmail.com>
2022-11-02 11:46:15 -0700

    feat: badge styles in offense chart

--- src/components/table/TeamTypeChart/index.tsx
+++ src/components/table/TeamTypeChart/index.tsx
@@ -25,14 +25,58 @@ function multiplierToBgClass(multiplier: ExtendedTypeEffectiveness) {
   }
 }
 
+function typeToBgClass(type: string) {
+  switch (type.toLowerCase()) {
+    case 'normal':
+      return 'bg-types-normal';
+    case 'fire':
+      return 'bg-types-fire';
+    case 'water':
+      return 'bg-types-water';
+    case 'electric':
+      return 'bg-types-electric';
+    case 'grass':
+      return 'bg-types-grass';
+    case 'ice':
+      return 'bg-types-ice';
+    case 'fighting':
+      return 'bg-types-fighting';
+    case 'poison':
+      return 'bg-types-poison';
+    case 'ground':
+      return 'bg-types-ground';
+    case 'flying':
+      return 'bg-types-flying';
+    case 'psychic':
+      return 'bg-types-psychic';
+    case 'bug':
+      return 'bg-types-bug';
+    case 'rock':
+      return 'bg-types-rock';
+    case 'ghost':
+      return 'bg-types-ghost';
+    case 'dragon':
+      return 'bg-types-dragon';
+    case 'dark':
+      return 'bg-types-dark';
+    case 'steel':
+      return 'bg-types-steel';
+    case 'fairy':
+      return 'bg-types-fairy';
+    default:
+      return '';
+  }
+}
+
 function Cell({ direction, id }: { direction: 'offense' | 'defense'; id: string }) {
   if (direction === 'defense') return <PokemonIcon speciesId={id} />;
+  // offense
   const move = DexSingleton.getGen().moves.get(id);
   if (!move) return null;
 
   // TODO: STAB bold
   return (
-    <span className="badge">
+    <span className={`m-1 badge badge-sm ${typeToBgClass(move.type)} text-base-100`}>
       {typesWithEmoji.find((t) => t.value === move.type)?.emoji}
       {move.name}
     </span>
@@ -65,7 +109,7 @@ export function TeamTypeChart<T extends ExtendedTypeEffectiveness | TypeEffectiv
             </td>
             {multipliers.map((multiplier) => (
               <td key={multiplier} className={`border-l-2 border-base-content/30 ${multiplierToBgClass(multiplier)}`}>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                <div className={`grid grid-cols-1 ${direction === 'offense' ? '' : 'sm:grid-cols-2 lg:grid-cols-3'}`}>
                   {mul2Ids[multiplier].map((id) => (
                     <Cell key={id} direction={direction} id={id} />
                   ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,31 @@ module.exports = {
           700: '#2b6cb0',
           800: '#2c5282',
           900: '#2a4365'
+        },
+        types: {
+          normal: '#A8A77A',
+          fire: '#EE8130',
+          water: '#6390F0',
+          electric: '#F7D02C',
+          grass: '#7AC74C',
+          ice: '#96D9D6',
+          fighting: '#C22E28',
+          poison: '#A33EA1',
+          ground: '#E2BF65',
+          flying: '#A98FF3',
+          psychic: '#F95587',
+          bug: '#A6B91A',
+          rock: '#B6A136',
+          ghost: '#735797',
+          dragon: '#6F35FC',
+          dark: '#705746',
+          steel: '#B7B7CE',
+          fairy: '#D685AD'
+        },
+        categories: {
+          physical: '#F08030',
+          special: '#6890F0',
+          status: '#7E7E7E'
         }
       }
     }
