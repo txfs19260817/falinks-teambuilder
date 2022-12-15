7510031c1551ab5f84bdf322cb38b730723ba879
txfs19260817 <duola865@gmail.com>
2022-12-14 16:12:21 -0800

    fix: add translations to pokemon icons

--- src/components/icons/PokemonIcon/index.tsx
+++ src/components/icons/PokemonIcon/index.tsx
@@ -1,4 +1,12 @@
 import type { ID } from '@pkmn/data';
 import { Icons } from '@pkmn/img';
+import { useTranslation } from 'next-i18next';
 
-export const PokemonIcon = ({ speciesId }: { speciesId: ID | string }) => <span title={speciesId} style={Icons.getPokemon(speciesId).css} />;
+import DexSingleton from '@/models/DexSingleton';
+
+export const PokemonIcon = ({ speciesId }: { speciesId: ID | string }) => {
+  const { t } = useTranslation();
+  const num = DexSingleton.getGen().species.get(speciesId)?.num;
+  const translatedName = t(num, { ns: 'species' }) || speciesId;
+  return <span role="img" title={translatedName} style={Icons.getPokemon(speciesId).css} aria-label={translatedName} />;
+};
