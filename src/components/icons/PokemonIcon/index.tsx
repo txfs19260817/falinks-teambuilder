8fc5dafcf697504df45e7fc9a6a602f6d59b7175
txfs19260817 <duola865@gmail.com>
2022-12-16 12:41:54 -0800

    fix: icons

--- src/components/icons/PokemonIcon/index.tsx
+++ src/components/icons/PokemonIcon/index.tsx
@@ -8,5 +8,17 @@ export const PokemonIcon = ({ speciesId }: { speciesId: ID | string }) => {
   const { t } = useTranslation();
   const num = DexSingleton.getGen().species.get(speciesId)?.num;
   const translatedName = t(num, { ns: 'species' }) || speciesId;
-  return <span role="img" title={translatedName} style={Icons.getPokemon(speciesId).css} aria-label={translatedName} />;
+  return (
+    <span
+      role="img"
+      title={translatedName}
+      style={
+        Icons.getPokemon(speciesId, {
+          protocol: 'https',
+          domain: 'raw.githubusercontent.com/MK-404/Giudizio-showdown-sprites/30a337a0d5e0c28035517c1a0eeb133d73347b2d',
+        }).css
+      }
+      aria-label={translatedName}
+    />
+  );
 };
