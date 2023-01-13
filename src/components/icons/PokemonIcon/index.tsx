33501119278f37b233608adc58a175e5cae03600
txfs19260817 <duola865@gmail.com>
2023-01-13 15:26:00 -0800

    feat: new icons

--- src/components/icons/PokemonIcon/index.tsx
+++ src/components/icons/PokemonIcon/index.tsx
@@ -8,5 +8,16 @@ export const PokemonIcon = ({ speciesId }: { speciesId: ID | string }) => {
   const { t } = useTranslation();
   const num = DexSingleton.getGen().species.get(speciesId)?.num;
   const translatedName = t(num, { ns: 'species' }) || speciesId;
-  return <span role="img" title={translatedName} style={Icons.getPokemon(speciesId).css} aria-label={translatedName} />;
+  return (
+    <span
+      role="img"
+      title={translatedName}
+      style={
+        Icons.getPokemon(speciesId, {
+          domain: 'www.falinks-teambuilder.com/assets/',
+        }).css
+      }
+      aria-label={translatedName}
+    />
+  );
 };
